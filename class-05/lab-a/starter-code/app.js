f7d37d7d3413ba7a9d41b63fda51e5fc883a8c7e
dianestephani <diane.stephani@gmail.com>
2020-05-14 21:21:41 -0700

    problem 3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -54,11 +54,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var secondSum = sum(a, b, c);
+  var secondProduct = multiply(a, b, c);
+  var secondSumString = a + ' and ' + b + ' and ' + c ' sum to ' + secondSum;
+  var secondProductString = 'The product of ' + a + ' and ' + b + ' and ' + c ' is ' + secondProduct + '.';
+  return[secondSum, secondProduct, secondSumString, secondProductString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+sumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
